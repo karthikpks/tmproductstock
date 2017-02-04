aaaa2e9d44fd43c59bf206581b5e854f9f2bfe7d
karthikpks <karthikparameswarankpm@gmail.com>
2017-02-04 23:20:47 +0530

    android code base initial commit

---
+++ app/src/main/java/com/tripperme/tmpstock/TripperMeProductStockActivity.java
@@ -0,0 +1,15 @@
+package com.tripperme.tmpstock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tripperme.tmpstock.tmpstock.R;
+
+public class TripperMeProductStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tripper_me_product_stock);
+    }
+}
