b1705a235626d990c042b3e6f15d01db42afcaee
karthikpks <karthikparameswarankpm@gmail.com>
2017-02-07 19:42:29 +0530

    #TMPS_03 Product Flavors commit

--- app/src/main/java/com/tripperme/tmpstock/util/Constants.java
+++ app/src/main/java/com/tripperme/tmpstock/util/Constants.java
@@ -1,14 +1,16 @@
 package com.tripperme.tmpstock.util;
 
+import com.tripperme.tmpstock.BuildConfig;
+
 /**
  * Created by karthik on 2/5/17.
  */
 
 public class Constants {
     public static final String FLAG_CURRENTLY_SELECTED_TAB_INDEX = "flagCurrentlySelectedTabIndex";
-    public static final String PARSE_APP_ID = "VzsxpWGqNqWeQagdQNKCTyIUv63ZEiSUVRDyt1w2";
-    public static final String PARSE_CLIENT_ID = "e6hjegfE1k5DmpWFV6TjkyCelUp8ZdZjefVJTYzX";
-    public static final String PARSE_SERVER = "https://parseapi.back4app.com/";
+    public static final String PARSE_APP_ID = BuildConfig.PARSE_APP_ID;
+    public static final String PARSE_CLIENT_ID = BuildConfig.PARSE_CLIENT_ID;
+    public static final String PARSE_SERVER = BuildConfig.PARSE_SERVER;
     public static final String PAGE_TITLE = "pageTitle";
     public static final String [] TAB_PAGE_TITLE_PRODUCTS = { "PRODUCTS", "CONTACTS", "TODAY" };
     public static final String FLAG_DIALOG_IS_SHOWN = "flagDialogIsShown";
